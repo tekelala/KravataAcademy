9c0634c86b7001bd3ce6b10813607d9d7e6e10f7
tekelala <camilo@tekelala.com>
2023-05-06 22:00:00 -0500

    5 containers

--- app.py
+++ app.py
@@ -33,14 +33,12 @@ if "prompts" not in st.session_state:
 if "class_generated" not in st.session_state:
     st.session_state.class_generated = False
 
-if "rerun" not in st.session_state:
-    st.session_state.rerun = False
-
-
+# Container 1: Title and banner image
 with st.container():
     st.title("Chat with the Kravata Teacher")
     st.markdown("Welcome to the class designer!")
 
+# Container 2: Enter the topic for the class
 with st.container():
     if not st.session_state.class_generated:
         with st.form(key='message_form'):
@@ -53,7 +51,53 @@ with st.container():
                     "role": "Human",
                     "content": f"""You are a seasoned teacher with the goal to impact your students allowing them to understand and engage. As an AI developed by Kravata, a company with the purpose of '{company_purpose}', I need you to generate a structure for a class on the topic of '{user_topic}'. The class should be aimed at beginners in the field of Web3. Please remember to use simple, easy-to-understand language and provide a clear outline of the class with key learning points. Keep the class short and impactful."""
                 })
-    else:
+# Container 3: Show the answer by Claude
+with st.container():
+    if st.session_state.prompts and user_topic and submit_button:
+        with st.spinner('Waiting for the Kravata Teacher...'):
+            try:
+                result = send_message(st.session_state.prompts)
+
+                # Append Claude's response to the prompts
+                st.session_state.prompts.append({
+                    "role": "Assistant",
+                    "content": result['completion']
+                })
+
+                # Display a success message
+                st.success("Message sent successfully!")
+
+                # Mark the class as generated only if there was a user message
+                if submit_button and user_topic:
+                    st.session_state.class_generated = True
+
+                # Display the entire conversation
+                for i, prompt in enumerate(st.session_state.prompts):
+                    if prompt['role'] == 'Human' and i != 0:
+                        st.write(f"You: {prompt['content']}")
+                    elif prompt['role'] == 'Assistant':
+                        st.write(f"Kravata Teacher: {prompt['content']}")
+
+            except requests.exceptions.HTTPError as errh:
+                st.error(f"HTTP Error: {errh}")
+            except requests.exceptions.ConnectionError as errc:
+                st.error(f"Error Connecting: {errc}")
+            except requests.exceptions.Timeout as errt:
+                st.error(f"Timeout Error: {errt}")
+            except requests.exceptions.RequestException as err:
+                st.error(f"Something went wrong: {err}")
+            except Exception as e:
+                st.error(f"Unexpected error: {e}")
+
+# Container 4: Reset button
+with st.container():
+    if st.button('Reset'):
+        st.session_state.prompts = []
+        st.session_state.class_generated = False
+
+# Container 5: Which part of the class do you want to develop
+with st.container():
+    if st.session_state.class_generated:
         with st.form(key='message_form'):
             user_message = st.text_input("Which part of the class do you want to develop?", key=f"user_input_{len(st.session_state.prompts)}")
             submit_button = st.form_submit_button(label='Send')
@@ -63,54 +107,5 @@ with st.container():
                     "role": "Human",
                     "content": user_message
                 })
-
-
-with st.container():
-    if st.session_state.prompts:
-        if submit_button and (user_topic or user_message):
-            with st.spinner('Waiting for the Kravata Teacher...'):
-                try:
-                    result = send_message(st.session_state.prompts)
-
-                    # Append Claude's response to the prompts
-                    st.session_state.prompts.append({
-                        "role": "Assistant",
-                        "content": result['completion']
-                    })
-
-                    # Display a success message
-                    st.success("Message sent successfully!")
-
-                    # Mark the class as generated only if there was a user message
-                    if submit_button and (user_topic or user_message):
-                        st.session_state.class_generated = True
-
-                except requests.exceptions.HTTPError as errh:
-                    st.error(f"HTTP Error: {errh}")
-                except requests.exceptions.ConnectionError as errc:
-                    st.error(f"Error Connecting: {errc}")
-                except requests.exceptions.Timeout as errt:
-                    st.error(f"Timeout Error: {errt}")
-                except requests.exceptions.RequestException as err:
-                    st.error(f"Something went wrong: {err}")
-                except Exception as e:
-                    st.error(f"Unexpected error: {e}")
-
-if st.session_state.rerun:
-    st.session_state.rerun = False  # Reset the rerun state
-    st.experimental_rerun()
-
-with st.container():
-    # Display the entire conversation
-    for i, prompt in enumerate(st.session_state.prompts):
-        if prompt['role'] == 'Human' and i != 0:
-            st.write(f"You: {prompt['content']}")
-        elif prompt['role'] == 'Assistant':
-            st.write(f"Kravata Teacher: {prompt['content']}")
-
-
-with st.container():
-    if st.button('Restart'):
-        st.session_state.prompts = []
-        st.session_state.class_generated = False
-        st.experimental_rerun()
+                # Re-run the script after appending new user_message to prompts
+                st.experimental_rerun()
